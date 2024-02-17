8ca15aa732c58a2a11f2bb8fe3e0b17f9f7121c1
Michael Gehrtz <63627744+mgehrtz@users.noreply.github.com>
2024-02-16 23:36:08 -0600

    Initial commit.

---
+++ the-gift-lms.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+* Plugin Name: The Gift - Learning Management System
+* Description: A plugin to manage courses and add functionality to The Gift.
+* Version: 0.0.1
+* Author: Gehrtz Creative
+* /
+
